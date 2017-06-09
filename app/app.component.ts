c051219b07e9c57d7d89fd02deb291a28667f4fb
Alexandr Kravchenko <alex.krava@live.com>
2017-06-09 15:37:08 +0300

    add task 1

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
 
+const VALUES = [
+    "Hello World",
+    "Привет Мир",
+    "Привіт Світ",
+    "Hola Mundo",
+    "Bonjour le monde",
+    "Hallo Welt",
+    "Ciao mondo",
+    "Witaj świecie",
+    "Hej världen",
+    "Pozdravljen, svet",
+    "Прывітанне Сусвет"];
+
 @Component({
     selector: 'app',
-    template: '<h1>My First Angular App</h1>'
+    templateUrl: './app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+    greetings: any;
+
+    constructor() {
+        this.greetings = VALUES;
+    }
+}
